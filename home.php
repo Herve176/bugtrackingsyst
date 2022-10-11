a0e7fc1ec0c709c514775f0469acc5be6d567d48
Herve176 <78292511+Herve176@users.noreply.github.com>
2022-10-11 16:13:11 +0200

    nothing

--- home.php
+++ home.php
@@ -4,8 +4,6 @@
 <html>
 <head>
      <title>Bug tracking sytem</title>
-tr:hover {background-color: #D6EEEE;}
-
 
 </head>
 <body>
@@ -28,11 +26,11 @@ tr:hover {background-color: #D6EEEE;}
  $sql="SELECT * FROM Bug;";
  $result=mysqli_query($conn,$sql);
  $resultcheck=mysqli_num_rows($result);
- if($resultcheck->num_rows>0){
+// if($resultcheck->num_rows>0){
     while($row=mysqli>fetch_assoc($result)){
         echo "<tr><td>".$row["idBug"]."<tr><td>".$row["idBugSeverity"]."<tr><td>".$row["idBugPriority"]."<tr><td>".$row["idBugstatus"]."<tr><td>".$row["idAccount"]."<tr><td>".$row["date_reported"]."<tr><td>".$row["summary"]."<tr><td>".$row["description"]."<tr><td>".$row["resolution"]."<tr><td>".$row["hours"]."<tr><td>".$row["reported_by"]."<tr><td>".$row["assigned_to"]."<tr><td>".$row["verified_by"]."</td></tr>";
     }
- }else{ echo "noresult";}
+ //}else{ echo "noresult";}
  $conn->close();
 ?>
 
